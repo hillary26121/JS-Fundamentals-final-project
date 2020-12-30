469c5b59e01aa8d13e936f4effb956b8b87a0f80
hillary26121 <hillaryywallace@gmail.com>
2020-12-30 18:37:58 -0500

    added placeholder image

--- JS/main.js
+++ JS/main.js
@@ -3,15 +3,15 @@
 (function () {
   let userInput = document.querySelector("#user-input");
   let searchResults = document.querySelector("#search-results");
+  let appDescription = document.querySelector("#app-description");
 
-
-  async function addMovie(id){
+//This function pulls the 'id' of each movie for more info, posters, and IMDB links and creates the HTML elements and their classes/attributes and appends them to the page.
+  
+async function addMovie(id){
+  try {
     let movieDetails = await axios.get(
       `https://api.themoviedb.org/3/movie/${id}?api_key=4e835435a3ded0ecd3bef3da14dbf091&language=en-US`
     )
-
-    console.log(movieDetails.data);
-    console.log(movieDetails.data.imdb_id);
 
     let curMovieInfo = document.createElement("div");
     curMovieInfo.setAttribute("class", "movie-info");
@@ -28,16 +28,28 @@
     curTitle.textContent = movieDetails.data.original_title;
 
     curAnchor.appendChild(curTitle);
-
     let curImage = document.createElement("img");
-    curImage.setAttribute("class", "movie-poster");
-    curImage.setAttribute(
-      "src",
-      `https://image.tmdb.org/t/p/w500${movieDetails.data.poster_path}`
-    );
-
+if(movieDetails.data.poster_path){
+  
+  curImage.setAttribute("class", "movie-poster");
+  curImage.setAttribute(
+    "src",
+    `https://image.tmdb.org/t/p/w500${movieDetails.data.poster_path}`,
+    
+  );
+} else {
+  curImage.setAttribute("src", 'https://via.placeholder.com/420x630.png')
+  curImage.setAttribute("class", "movie-poster")
+}
+   
+    console.log(movieDetails.data.poster_path)
     curAnchor.appendChild(curImage);
+  } catch (error) {
+    appDescription.textContent = "Whoops, looks like there's a problem. We are working on ourselves right now, try again later. It's not you, it's us."
+    console.error(error)
   }
+}
+
   //this function calls the API to return search results based on user input
 
   userInput.addEventListener("input", () => {
@@ -50,7 +62,9 @@
         let queries = searchResponse.data.results;
         queries.forEach((query) => {
           addMovie(query.id)
+         
         });
+       
 
       } catch (error) {
         console.error(error);
